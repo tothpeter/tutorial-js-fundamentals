3361cfd559a0ca87a414003e3af60eddeb8ccdd6
Peter Toth <tothpeter08@gmail.com>
2015-05-02 13:54:14 +0200

    2.2 Data Types

---
+++ 02_basics/js/basics.js
@@ -0,0 +1,28 @@
+// Data types (loosly typed)
+
+// Primitives
+// # Number
+console.log(isNaN("A"));
+
+// # String
+// # Boolean
+
+// ## Falsey
+// false, 0, '', null, undefined, NaN
+// ## Truthy
+// All others
+
+// Special and almost similar data types
+// Undefined -> can't be used for value
+// Null -> datatype, can be used as value
+
+// Composite data type
+// Array, Object
+
+obj = {
+  asd: 1,
+  d: 2
+}
+
+obj.asd;
+obj['asd'];
