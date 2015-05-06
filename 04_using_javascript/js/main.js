ff0a9bd7c6a1f0bccec9497d2822952cab937ded
Peter Toth <tothpeter08@gmail.com>
2015-05-06 23:32:31 +0200

    4.1 Working With Strings

---
+++ 04_using_javascript/js/main.js
@@ -0,0 +1,14 @@
++function(){
+  'use strict';
+
+  var testString = 'test string sa la la';
+
+  // console.log(testString.split(' '));
+  // console.log(testString.indexOf('t', 1));
+  // console.log(testString.lastIndexOf('t'));
+
+  // console.log(testString.substr(0, 4));
+  // console.log(testString.substr(-2));
+  // console.log(testString.slice(0, 4));
+  // console.log("    a ".trim());
+}();
