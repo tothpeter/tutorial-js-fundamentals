82b242d359d9783329ecb7b82f70b5a261953a6b
Peter Toth <tothpeter08@gmail.com>
2015-05-09 15:56:29 +0200

    4.6 Working With Dates and Times

---
+++ 04_using_javascript/js/dates.js
@@ -0,0 +1,28 @@
++function(){
+  'use strict';
+
+  var now = new Date();
+  // console.log(now);
+
+  var partyTime = new Date(946684799000-1000*60*60);
+  // console.log(partyTime);
+
+  var dob = new Date('1988 09 29');
+  // var dob = new Date(1988, 8, 29);
+  // console.log(dob);
+  // console.log(dob.getDate());
+  // console.log(dob.getMonth());
+  // console.log(dob.getFullYear());
+
+  // console.log(dob.getUTCDate());
+  // console.log(dob.getUTCMonth());
+  // console.log(dob.getUTCFullYear());
+
+  // console.log(dob.getUTCDay());
+
+  console.log(dob.toDateString());
+  console.log(dob.toTimeString());
+  console.log(dob.toISOString());
+
+  console.log(dob.toLocaleString());
+}();
