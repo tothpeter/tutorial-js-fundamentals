97e9880a2374f76ac9a2517abe8dcf8f8daa1567
Peter Toth <tothpeter08@gmail.com>
2015-05-10 23:55:12 +0200

    6.4 The History API

---
+++ 06_web_apis/js/history.js
@@ -0,0 +1,43 @@
++function(){
+  'use strict';
+
+  function loadPage(xhr, urlToLoad) {
+    xhr.onload = function() {
+      var response = xhr.response;
+
+      parseAndInsertPage(response);
+
+      window.history.pushState({ pageContent: response }, 'JS Fnd', urlToLoad);
+    }
+
+    xhr.open('GET', urlToLoad, true);
+    xhr.send();
+  }
+
+  function parseAndInsertPage(pageString) {
+    var bodyTemp = pageString.split('</head>')[1] || pageString,
+        body = bodyTemp.split('</html>')[0] || pageString;
+
+    document.body.outerHTML = body;
+  }
+
+  document.addEventListener('click', function(e) {
+
+    if (e.target.nodeName === 'A') {
+      e.preventDefault();
+
+      var xhr = new XMLHttpRequest,
+          newUrl = e.target.href;
+      
+      loadPage(xhr, newUrl);
+
+      window.history.pushState({ pageContent: document.body.outerHTML}, 'Js FUnd1', document.location.href)
+    }
+  });
+
+  window.addEventListener('popstate', function(e) {
+    if (e.state && typeof e.state.pageContent === 'string') {
+      parseAndInsertPage(e.state.pageContent);
+    }
+  })
+}();
