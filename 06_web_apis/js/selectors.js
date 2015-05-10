276fb9deb4530bcf4dc439a3b7158420c3dfc3ce
Peter Toth <tothpeter08@gmail.com>
2015-05-10 22:12:25 +0200

    6.1 Selector API

---
+++ 06_web_apis/js/selectors.js
@@ -0,0 +1,9 @@
++function(){
+  'use strict';
+
+  var mainHeading = document.querySelector('h1');
+  // console.log(mainHeading);
+
+  var headers = document.querySelectorAll('header');
+  console.log(headers);
+}();
