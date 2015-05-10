2768050f3a43dfcc92c521d6867df2ef772c2d63
Peter Toth <tothpeter08@gmail.com>
2015-05-10 18:51:58 +0200

    5.1 Selecting HTML Elements

---
+++ 05_working_with_the_dom/js/elements.js
@@ -0,0 +1,8 @@
++function(){
+  'use strict';
+
+  // console.log(document.getElementById('mainHeading'));
+  // console.log(document.getElementsByClassName('class1'));
+  // console.log(document.getElementsByTagName('div'));
+  console.log(document.getElementsByName('div'));
+}();
