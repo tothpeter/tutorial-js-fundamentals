70305fbd8596e8884f0c62b72700f6fe4cbdadb6
Peter Toth <tothpeter08@gmail.com>
2015-05-12 22:17:33 +0200

    7.5 Regular Expressions Continued

--- 7_advanced/js/regex.js
+++ 7_advanced/js/regex.js
@@ -12,6 +12,20 @@
 
   // Alphanum
   var noSpecialChar = /^\w*$/;
-  console.log(noSpecialChar.test('a_z1-9'));
+  // console.log(noSpecialChar.test('a_z1-9'));
 
+
+  var pattern = /^https?:/;
+  // console.log(pattern.test('https://asd.asd'));
+
+  var camelCased = 'CamelCasedString';
+  // console.log(camelCased.replace(/([A-Z])/g,'_$1').toLowerCase().substr(1));
+
+  // non capturing group: /(?:A-Z)/
+
+  var pattern = /java(?!script)/i;
+  // console.log(pattern.test('Java'));
+
+  var lettersAndNumbers = /\d/g;
+  console.log('asd67'.match(lettersAndNumbers));
 }();
